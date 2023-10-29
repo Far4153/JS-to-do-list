42b05a2ce84169492a221b4397963f7f78f180c4
farjana <farjanashaik153@gmail.com>
2023-10-29 22:40:50 +0530

    deletebin

--- first.js/index.js
+++ first.js/index.js
@@ -8,10 +8,29 @@ function displayText() {
 
         var uncheckedImage=document.createElement('img');
         uncheckedImage.src="images/radio-unchecked-2.png";
-        listItem.appendChild(uncheckedImage);
+        
+        uncheckedImage.addEventListener("click", function(){
+            if (uncheckedImage.src.includes("images/radio-unchecked-2.png")){
+                uncheckedImage.src="images/checked-1.png";
+                
+            }else{
+                uncheckedImage.src= "images/radio-unchecked-2.png";
+            }
+        })
+        
+        var deleteBin= document.createElement("img");
+        deleteBin.src="images/delete-bin.png";
+        deleteBin.setAttribute('id', 'deletebin')
 
+        
         var textTask= document.createTextNode(text);
-        listItem.appendChild(textTask);
+        var textDiv = document.createElement('div');
+        textDiv.setAttribute('class', 'textDiv'); // Use 'textDiv' with a capital 'D'
+        textDiv.appendChild(textTask);
+        
+        listItem.appendChild(uncheckedImage);
+        listItem.appendChild(textDiv);
+        listItem.appendChild(deleteBin);
 
         outputElement.appendChild(listItem);
 
