bc5edbe86a91e02fc66675830959835ea09a81e7
farjana <farjanashaik153@gmail.com>
2023-10-30 12:51:30 +0530

    event delegation

--- first.js/index.js
+++ first.js/index.js
@@ -20,8 +20,13 @@ function displayText() {
         
         var deleteBin= document.createElement("img");
         deleteBin.src="images/delete-bin.png";
-        deleteBin.setAttribute('id', 'deletebin')
+        deleteBin.setAttribute('id', 'deletebin');
 
+        deleteBin.addEventListener('click', function(){
+            var listdel = this.parentElement;
+            var todolist = document.getElementById('outputText');
+            todolist.removeChild(listdel);
+        })
         
         var textTask= document.createTextNode(text);
         var textDiv = document.createElement('div');
@@ -34,11 +39,59 @@ function displayText() {
 
         outputElement.appendChild(listItem);
 
-        inputElement.value="";
+        // var deleteimg=document.getElementById('deletebin');
+        // deleteimg.onclick= function(){
+        //     var listdel= this.parentElement;
+
+        //     var todolist=document.getElementById('outputText');
+        //     todolist.removeChild(listdel);
+        // }
         
+        inputElement.value="";
+        updateLocalStorage();
     }
 
 }
+
+document.getElementById("outputText").addEventListener("click", function (event) {
+    var target = event.target;
+    
+    // Check if the clicked element is an unchecked image
+    if (target.tagName === "IMG" && target.src.includes("radio-unchecked-2.png")) {
+        target.src = "images/checked-1.png";
+        updateLocalStorage();
+    } else if (target.tagName === "IMG" && target.src.includes("checked-1.png")) {
+        target.src = "images/radio-unchecked-2.png";
+        updateLocalStorage();
+    }
+
+    // Check if the clicked element is a delete bin image
+    if (target.tagName === "IMG" && target.src.includes("delete-bin.png")) {
+        var listdel = target.parentElement;
+        var todolist = document.getElementById('outputText');
+        todolist.removeChild(listdel);
+        updateLocalStorage();
+    }
+});
+
+function updateLocalStorage(){
+    var outputElement= document.getElementById('outputText');
+    localStorage.setItem('todoTasks', outputElement.innerHTML);
+}
+
+function LoadFromLocalStorage(){
+    var storedtasks=localStorage.getItem('todoTasks');
+    if (storedtasks){
+
+        var outputElement = document.getElementById("outputText");
+        outputElement.innerHTML=storedtasks;
+    }
+}
+
+
+window.addEventListener("load", function () {
+    LoadFromLocalStorage();
+});
 
 // Listen for the Enter key press in the input field
 
